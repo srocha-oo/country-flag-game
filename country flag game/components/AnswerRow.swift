49f57aba652e8bee8e94a5bf5a12b4f2a63feaf6
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-01-20 10:32:51 -0600

    created progress bar

---
+++ country flag game/components/AnswerRow.swift
@@ -0,0 +1,18 @@
+//
+//  answer row.swift
+//  country flag game
+//
+//  Created by stephanie rocha marquez on 1/16/26.
+//
+
+import SwiftUI
+
+struct answer_row: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    answer_row()
+}
